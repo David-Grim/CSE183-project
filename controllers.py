7fae55303bcb100ecf186d2ea0aba44aea64b34b
ali malik <alimalik2000@gmail.com>
2021-05-11 11:07:02 -0700

    Users now have to login when they first open the website

--- controllers.py
+++ controllers.py
@@ -28,9 +28,14 @@ Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app w
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from py4web.utils.url_signer import URLSigner
+from .models import get_user_email
 
+url_signer = URLSigner(session)
 
-@unauthenticated("index", "index.html")
+@action('index')
+@action.uses(db, auth.user, 'index.html')
+#@action.uses(db, auth.user, 'index.html')
 def index():
     user = auth.get_user()
     message = T("Hello {first_name}".format(**user) if user else "Hello")
