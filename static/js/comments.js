821104b4eccb29d64d8ddf3114f4dedb11988095
Mark-Krause <markkrause1@gmail.com>
2021-05-27 18:42:42 +0000

    recursive test code

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,3 +1,57 @@
+let tree = {
+  label: 'root',
+  nodes: [
+    {
+      label: 'item1',
+      nodes: [
+        {
+          label: 'item1.1'
+        },
+        {
+          label: 'item1.2',
+          nodes: [
+            {
+              label: 'item1.2.1'
+            }
+          ]
+        }
+      ]
+    }, 
+    {
+      label: 'item2'  
+    }
+  ]
+}
+
+Vue.component('tree-menu', { 
+  template: '#tree-menu',
+  props: [ 'nodes', 'label', 'depth' ],
+  data() {
+     return {
+       showChildren: false
+     }
+  },
+  computed: {
+    iconClasses() {
+      return {
+        'fa-plus-square-o': !this.showChildren,
+        'fa-minus-square-o': this.showChildren
+      }
+    },
+    labelClasses() {
+      return { 'has-children': this.nodes }
+    },
+    indent() {
+      return { transform: `translate(${this.depth * 50}px)` }
+    }
+  },
+  methods: {
+    toggleChildren() {
+       this.showChildren = !this.showChildren;
+    }
+  }
+});
+
 // This will be the object that will contain the Vue attributes
 // and be used to initialize it.
 let app = {};
@@ -13,6 +67,7 @@ let init = (app) => {
         post_text: "",
         add_mode: false,
         reply_id: -1,
+        tree
     };
 
     app.enumerate = (a) => {
